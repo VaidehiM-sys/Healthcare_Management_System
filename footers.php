222c403e68784822553859c4d7f25c3b2b7d9367
VaidehiM-sys <52694641+VaidehiM-sys@users.noreply.github.com>
2021-03-10 21:23:46 -0800

    Update footers.php

--- footers.php
+++ footers.php
@@ -9,41 +9,7 @@
 <body>
 
 <footer class="container py-5"">
-  <div class="row">
-    <div class="col-12 col-md">
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="d-block mb-2" role="img" viewBox="0 0 24 24" focusable="false"><title>Product</title><circle cx="12" cy="12" r="10"/><path d="M14.31 8l5.74 9.94M9.69 8h11.48M7.38 12l5.74-9.94M9.69 16L3.95 6.06M14.31 16H2.83m13.79-4l-5.74 9.94"/></svg>
-      <small class="d-block mb-3 text-muted">&copy; 2019-2020</small>
-    </div>
-    <div class="col-6 col-md">
-      <h5>Features</h5>
-      <ul class="list-unstyled text-small">
-        <li><a class="text-muted" href="#">24/7 website Support</a></li>
-        <li><a class="text-muted" href="#">Best Doctors in World</a></li>
-        <li><a class="text-muted" href="#">Team feature</a></li>
-        <li><a class="text-muted" href="#">Website Support</a></li>
-        <li><a class="text-muted" href="#">Least Cost Treatments</a></li>
-      </ul>
-    </div>
-    <div class="col-6 col-md">
-      <h5>Resources</h5>
-      <ul class="list-unstyled text-small">
-        <li><a class="text-muted" href="#">Doctors</a></li>
-        <li><a class="text-muted" href="#">Nurses</a></li>
-        <li><a class="text-muted" href="#">Support Team</a></li>
-        <li><a class="text-muted" href="#">Clinics</a></li>
-      </ul>
-    </div>
-    
-    <div class="col-6 col-md">
-      <h5>About</h5>
-      <ul class="list-unstyled text-small">
-        <li><a class="text-muted" href="#">Team</a></li>
-        <li><a class="text-muted" href="#">Locations</a></li>
-        <li><a class="text-muted" href="#">Privacy</a></li>
-        <li><a class="text-muted" href="#">Terms</a></li>
-      </ul>
-    </div>
-  </div>
+
 </footer>
     
 </body>
